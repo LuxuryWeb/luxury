e0d4b142e42d4ba4a7b63a8059cf36fb1d7bb944
Juan Segundo <juansepincha@gmail.com>
2024-04-21 22:35:14 -0300

    new changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,26 +5,9 @@ const Footer = () => {
   return (
     <footer className="w-full max-w-[1200px] mx-auto my-2">
       <div className="py-5 bg-zinc-800 rounded-3xl flex items-center justify-center flex-col sm:flex-row">
-        <div className="flex justify-center items-center">
-          {/* <button className="transform hover:scale-110 transition duration-500 ease-in-out p-2">
-            <Image
-              src="/icons/messanger.svg"
-              alt="logo"
-              width={30}
-              height={30}
-            ></Image>
-          </button>
-          <button className="transform hover:scale-110 transition duration-500 ease-in-out p-2">
-            <Image
-              src="/icons/twitter.svg"
-              alt="logo"
-              width={30}
-              height={30}
-            ></Image>
-          </button> */}
-        </div>
+        <div className="flex justify-center items-center"></div>
         <div className="flex gap-2">
-          <Image src="/imgs/logo.png" alt="logo" width={100} height={100} />
+          <Image src="/imgs/logo.png" alt="logo" width={170} height={170} />
           <button className="transform hover:scale-110 transition duration-500 ease-in-out p-2">
             <Image
               src="/icons/instagram.svg"
